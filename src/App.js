60b27dccc133e92dc44a45dfa3021ea15d7416ab
piterscl <pedro.antolin@gmail.com>
2021-01-05 05:21:45 +0000

    incluido fetch

--- src/App.js
+++ src/App.js
@@ -1,19 +1,72 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 import FormularioTareas from "./components/FormularioTarea";
 import ListaTarea from "./components/ListaTarea";
 
 function App() {
-  const [tareas, setTarea] = useState ([]);
+  const [tareas, setTarea] = useState ([{ label: "", done: false }]);
 
-  function  agregarTarea (tarea) {
+/*  function  agregarTarea (tarea) {
     setTarea([tarea,...tareas]);
-  }
+  }*/
  
   function eliminarTarea(id){
     setTarea(tareas.filter(tarea => tarea.id !== id));
   }
 
+const agregarTarea = () => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/piterscl", {
+			method: "GET",
+			headers: {
+				"content-type": "application/json"
+			}
+		})
+			.then(res => res.json())
+			.then(response => {
+				setTarea(
+					response.map((item, index) => {
+						return item;
+					})
+				);
+			});
+    };
+    const crearTarea = () => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/piterscl", {
+			method: "POST",
+			headers: {
+				"content-type": "application/json"
+			},
+			body: []
+		})
+			.then(response => {
+				console.log(response);
+			})
+			.catch(err => {
+				console.log(err);
+			});
+    };
+    
+    const updateTarea = () => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/piterslc", {
+			method: "PUT",
+			headers: {
+				"content-type": "application/json"
+			},
+			body: JSON.stringify(ListaTarea)
+		})
+			.then(response => {
+				console.log(response);
+			})
+			.catch(err => {
+				console.log(err);
+			});
+    };    
+
+    useEffect(() => {
+        agregarTarea();
+        updateTarea ();    
+        crearTarea ();
+	}, []);
   return (
     <div className="App">
       <header className="App-header">
